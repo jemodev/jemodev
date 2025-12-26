e248e7e64b84c8ed6fe99fd815eebdb982ed3889
Jerson Moreno <jjmoreno@pan-energy.com>
2025-12-26 17:05:24 -0400

    feat: Fetch and display dynamic 'About' content on the homepage, introduce a new WordPress schema, and adjust main layout styling.

---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+import { z } from "astro:content";
+
+export const BaseWPSchema = z.object({
+    id: z.number(),
+    title: z.object({ rendered: z.string() }),
+    content: z.object({ rendered: z.string() }),
+});
