6e739690461a5026454803ec04ad906842231b21
Jerson Moreno <jjmoreno@pan-energy.com>
2025-12-28 12:51:42 -0400

    feat: add image and feature image schemas and integrate them into the BaseWPSchema

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,24 @@
 import { z } from "astro:content";
 
+const imageSchema = z.object({
+    url: z.string().url(),
+    width: z.number(),
+    height: z.number(),
+});
+
+const featureImageSchema = z.object({
+    thumbnail: imageSchema,
+    medium: imageSchema,
+    medium_large: imageSchema,
+    large: imageSchema,
+    full: imageSchema,
+});
+
 export const BaseWPSchema = z.object({
     id: z.number(),
     title: z.object({ rendered: z.string() }),
     content: z.object({ rendered: z.string() }),
+    featured_images: featureImageSchema,
 });
 
 export const ExperienceItemSchema = BaseWPSchema.pick({
